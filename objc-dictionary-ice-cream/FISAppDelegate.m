866afafec31ef4155fe0f23c912a5f478e33be04
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-05-26 11:48:57 -0400

    Done.

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -16,4 +16,40 @@
  
  */
 
+- (NSArray *)namesForIceCream:(NSString *)iceCream {
+    
+    NSLog(@"This is the argument ice cream flavor: %@", iceCream);
+  
+    
+    NSDictionary *namesForIceCreamFlavor = @{ @"Joe"   : @"Peanut Butter and Chocolate",
+                                             @"Tim"    : @"Natural Vanilla"            ,
+                                             @"Sophie" : @"Mexican Chocolate"          ,
+                                             @"Deniz"  : @"Natural Vanilla"           ,
+                                             @"Tom"    : @"Mexican Chocolate"          ,
+                                             @"Jim"    : @"Natural Vanilla"            ,
+                                             @"Mark"   : @"Cookies 'n Cream"          };
+   
+    NSLog(@"This is the argument ice cream flavor two %@", namesForIceCreamFlavor);
+    
+    NSArray *namesForIceCream = [namesForIceCreamFlavor allKeysForObject:iceCream];
+    
+    
+    return namesForIceCream;
+
+    }
+
+- (NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName {
+    
+    NSMutableDictionary *numberOfPeopleWhoLikesThisFlavor = [[NSMutableDictionary alloc]init];
+    NSArray *iceCreamFlavor = [iceCreamByName allValues];
+    for (NSUInteger i = 0; i < [iceCreamFlavor count]; i++) {
+        NSArray *returnArrayIceCream = [self namesForIceCream:iceCreamFlavor[i]];
+        NSNumber *countNames = @([returnArrayIceCream count]);
+        numberOfPeopleWhoLikesThisFlavor[iceCreamFlavor[i]] = countNames;
+    }
+
+
+return numberOfPeopleWhoLikesThisFlavor;
+}
+
 @end
